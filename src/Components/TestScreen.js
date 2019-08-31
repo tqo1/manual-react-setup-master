40a2e104209421a7df67663d5a877a02e8ff98b4
tq-o <onquocthai1106@gmail.com>
2019-08-31 00:45:41 -0400

    try
    try

--- src/Components/TestScreen.js
+++ src/Components/TestScreen.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import styles from "./testscreen.css"
+<<<<<<< Updated upstream
+=======
+import Timer from './timer'
+import Canvas from './draw'
+>>>>>>> Stashed changes
 export default class TestScreen extends React.Component {
     
     render() {
         const {testData, submitHandler} = this.props
+<<<<<<< Updated upstream
         return <div>
             <h1>{testData.testTitle}  Time Limit: {testData.overallTimeInSec} seconds</h1>
             <div>
+=======
+        return <div className="appContainer">
+            <Timer/>
+            <Canvas/>
+            
+            <div className="testContainer">
+>>>>>>> Stashed changes
                 <div className="passageSection">
 
                 </div>
