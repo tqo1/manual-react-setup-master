55aa7e53750b30e5a4d529adb38bb3de0f4e771e
Vi <vivivi0701@gmail.com>
2019-09-30 21:42:17 +0200

    update popover

--- src/Components/TestScreen.js
+++ src/Components/TestScreen.js
@@ -1,33 +1,48 @@
 import React from 'react'
 import styles from "./testscreen.css"
-<<<<<<< Updated upstream
-=======
 import Timer from './timer'
->>>>>>> Stashed changes
+import Highlight from './getHighlight'
+import Multiplechoice from './button'
 export default class TestScreen extends React.Component {
     
     render() {
         const {testData, submitHandler} = this.props
-        return <div>
-            <h1>{testData.testTitle}  Time Limit: {testData.overallTimeInSec} seconds</h1>
-            <div>
+        return <div className="appContainer">
+            <Timer/>            
+            <div className="testContainer">
                 <div className="passageSection">
-<<<<<<< Updated upstream
-=======
-                    <div className='article'>
-                    
-                    </div>
+                <div className="passagename">
+                <b>
+                This passage is from James Joyce, The Dubliners originally published in 1914                
+                </b>
                 </div>
->>>>>>> Stashed changes
-
+                <Highlight/>
                 </div>
                 <div className="questionSection">
+                    <div className="article2">
+                    <Multiplechoice/>
+                    <p>At noon they sat down by the roadside, near a little brook, and Dorothy opened her basket and got out some bread.  She offered a piece to the Scarecrow, but he refused.</p>
+                    <p>&ldquo;I am never hungry,&rdquo; he said, &ldquo;and it is a lucky thing I am not, for my mouth is only painted, and if I should cut a hole in it so I could eat, the straw I am stuffed with would come out, and that would spoil the shape of my head.&rdquo;</p>
+                    <p>At noon they sat down by the roadside, near a little brook, and Dorothy opened her basket and got out some bread.  She offered a piece to the Scarecrow, but he refused.</p>
+                    <p>&ldquo;I am never hungry,&rdquo; he said, &ldquo;and it is a lucky thing I am not, for my mouth is only painted, and if I should cut a hole in it so I could eat, the straw I am stuffed with would come out, and that would spoil the shape of my head.&rdquo;</p>
+                    <p>&ldquo;I am never hungry,&rdquo; he said, &ldquo;and it is a lucky thing I am not, for my mouth is only painted, and if I should cut a hole in it so I could eat, the straw I am stuffed with would come out, and that would spoil the shape of my head.&rdquo;</p>
+                    <p>&ldquo;I am never hungry,&rdquo; he said, &ldquo;and it is a lucky thing I am not, for my mouth is only painted, and if I should cut a hole in it so I could eat, the straw I am stuffed with would come out, and that would spoil the shape of my head.&rdquo;</p>
+                    <p>&ldquo;I am never hungry,&rdquo; he said, &ldquo;and it is a lucky thing I am not, for my mouth is only painted, and if I should cut a hole in it so I could eat, the straw I am stuffed with would come out, and that would spoil the shape of my head.&rdquo;</p>
+                    <p>&ldquo;I am never hungry,&rdquo; he said, &ldquo;and it is a lucky thing I am not, for my mouth is only painted, and if I should cut a hole in it so I could eat, the straw I am stuffed with would come out, and that would spoil the shape of my head.&rdquo;</p>
+                    <p>&ldquo;I am never hungry,&rdquo; he said, &ldquo;and it is a lucky thing I am not, for my mouth is only painted, and if I should cut a hole in it so I could eat, the straw I am stuffed with would come out, and that would spoil the shape of my head.&rdquo;</p>
+                    <p>At noon they sat down by the roadside, near a little brook, and Dorothy opened her basket and got out some bread.  She offered a piece to the Scarecrow, but he refused.</p>
+                    <p>&ldquo;I am never hungry,&rdquo; he said, &ldquo;and it is a lucky thing I am not, for my mouth is only painted, and if I should cut a hole in it so I could eat, the straw I am stuffed with would come out, and that would spoil the shape of my head.&rdquo;</p>
+                    <p>At noon they sat down by the roadside, near a little brook, and Dorothy opened her basket and got out some bread.  She offered a piece to the Scarecrow, but he refused.</p>
+                    <p>&ldquo;I am never hungry,&rdquo; he said, &ldquo;and it is a lucky thing I am not, for my mouth is only painted, and if I should cut a hole in it so I could eat, the straw I am stuffed with would come out, and that would spoil the shape of my head.&rdquo;</p>
+                    <p>&ldquo;I am never hungry,&rdquo; he said, &ldquo;and it is a lucky thing I am not, for my mouth is only painted, and if I should cut a hole in it so I could eat, the straw I am stuffed with would come out, and that would spoil the shape of my head.&rdquo;</p>
+                    <p>&ldquo;I am never hungry,&rdquo; he said, &ldquo;and it is a lucky thing I am not, for my mouth is only painted, and if I should cut a hole in it so I could eat, the straw I am stuffed with would come out, and that would spoil the shape of my head.&rdquo;</p>
+                    <p>&ldquo;I am never hungry,&rdquo; he said, &ldquo;and it is a lucky thing I am not, for my mouth is only painted, and if I should cut a hole in it so I could eat, the straw I am stuffed with would come out, and that would spoil the shape of my head.&rdquo;</p>
+                    <p>&ldquo;I am never hungry,&rdquo; he said, &ldquo;and it is a lucky thing I am not, for my mouth is only painted, and if I should cut a hole in it so I could eat, the straw I am stuffed with would come out, and that would spoil the shape of my head.&rdquo;</p>
+                    <p>&ldquo;I am never hungry,&rdquo; he said, &ldquo;and it is a lucky thing I am not, for my mouth is only painted, and if I should cut a hole in it so I could eat, the straw I am stuffed with would come out, and that would spoil the shape of my head.&rdquo;</p>
 
+                    </div>
                 </div>
             </div>
-            <div>Jump to question:</div>
-            <div onClick={submitHandler}>Submit</div>
-
         </div>
     }
 }
---
+++ src/Components/getHighlight.js
@@ -0,0 +1,92 @@
+import React, { Component } from "react";
+import App from "./popover";
+import styles from "./testscreen.css"; 
+import {
+    usePopoverState,
+    Popover as BasePopover,
+    PopoverDisclosure,
+    PopoverArrow
+  } from "reakit/Popover";
+  
+
+export default class Highlight extends Component {
+    constructor(props) {
+        super(props);
+        this.saveSelection = this.saveSelection.bind(this);
+        this.myRef = React.createRef();
+      }
+
+
+      saveSelection = () => {
+        var range, sel;
+        if (window.getSelection) {
+            // Non-IE case
+            sel = window.getSelection();
+            if (sel.getRangeAt) {
+                <Popover
+                aria-label="Custom popover"
+                content={range = sel.getRangeAt(0)}
+              >
+                Add highlight
+              </Popover>
+            }
+            document.designMode = "on";
+            if (range) {
+                sel.removeAllRanges();
+                sel.addRange(range); 
+                <Popover
+                aria-label="Custom popover"
+                content={range = sel.getRangeAt(0)}
+              >
+                remove highlight
+              </Popover>
+            }
+            document.execCommand("italic", false, null);
+            document.designMode = "off";
+        } else if (document.selection && document.selection.createRange &&
+                document.selection.type != "None") {
+            // IE case
+            range = document.selection.createRange();
+            range.execCommand("italic", false, null);
+        }
+      }
+      render() {
+        return (
+        <div>
+        <div  className="highlight" onMouseUp={this.saveSelection}>
+        <p> 
+                    Mr Holohan, assistant secretary of the Eire
+            Abu Society, had been walking up and down
+            Dublin for nearly a month, with his hands and
+            pockets full of dirty pieces of paper, arranging
+            about the series of concerts. He had a game
+            leg and for this his friends called him Hoppy
+            Holohan. He walked up and down constantly,
+            stood by the hour at street corners arguing
+            the point and made notes; but in the end it
+            was Mrs Kearney who arranged everything
+        </p>
+        <p>
+                Miss Devlin had become Mrs Kearney out
+        of spite. She had been educated in a
+        high-class convent, where she had learned
+        French and music. As she was naturally pale
+        and unbending in manner she made few
+        friends at school. When she came to the age
+        of marriage she was sent out to many houses
+        where her playing and ivory manners were
+        much admired. She sat amid the chilly circle
+        of her accomplishments, waiting for some
+        suitor to brave it and oer her a brilliant life.
+        But the young men whom she met were Kathleen, to a good convent, where she
+        learned French and music, and aerward paid
+        her fees at the Academy. Every year in the
+        month of July Mrs Kearney found occasion to
+        say to some friend:
+        </p>
+        </div>
+        </div>
+        )
+      }
+    
+}
